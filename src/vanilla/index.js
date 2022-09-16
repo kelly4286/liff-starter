0c3a2a4a56e13b99ba8142192ecd2a476af12bff
Kelly <kelly.4286@hotmail.com>
2022-09-16 15:49:51 +0800

    first commit

---
+++ src/vanilla/index.js
@@ -0,0 +1,41 @@
+import './index.css';
+import liff from '@line/liff';
+
+document.addEventListener('DOMContentLoaded', function () {
+  liff
+    .init({
+      liffId: '1657419934-Jla7p0KM',
+      withLoginOnExternalBrowser: true, // Enable automatic login process
+    })
+    .then(() => {
+      console.log('Success! you can do something with LIFF API here.');
+
+      if (!liff.isInClient()) {
+        alert('Liff is NOT in line app.');
+      }
+
+      liff
+        .getProfile()
+        .then(function (profile) {
+          const userId = profile.userId;
+          const name = profile.displayName;
+          const pictureUrl = profile.pictureUrl;
+          const statusMessage = profile.statusMessage;
+
+          liff.getFriendship().then(function (friendshipStatus) {
+            document.body.innerHTML +=
+              '<div style="position:absolute;width:100%;height:100%;">' +
+              '使用者 Id: ' + userId + '<br/>' +
+              '使用者名稱: ' + name + '<br/>' +
+              'Status Message: ' + statusMessage + '<br/>' +
+              '綁定狀態: ' + (friendshipStatus.friendFlag ? '已加入好友' : '未加入好友') + '<br/>' +
+              '<img src="' + pictureUrl + '" height=100 width=100><br/>' +
+              '</div>';
+            liff.logout();
+
+          });
+        }).catch(function (error) {
+          console.log('error', error);
+        });
+    });
+});
